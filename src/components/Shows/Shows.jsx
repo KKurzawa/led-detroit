6b0ac051dd7c5e39d825e27ac374afbe0f5ae28c
Kris Kurzawa <kurzawa1@yahoo.com>
2024-05-22 20:43:58 -0400

    upadated shows header

--- src/components/Shows/Shows.jsx
+++ src/components/Shows/Shows.jsx
@@ -1,8 +1,10 @@
-import ',/Shows.css'
+import './Shows.css'
 
 const Shows = () => {
     return (
-        <main id='Shows'>Shows</main>
+        <main id='Shows' className='flex flex-col items-center justify-center'>
+            <header className='shows-header flex justify-center items-center my-3 text-4xl'>Shows</header>
+        </main>
     )
 }
 
