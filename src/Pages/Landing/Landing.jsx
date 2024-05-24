6a07941b1dc2099da8d90250f4718ff92cc697ef
Kris Kurzawa <kurzawa1@yahoo.com>
2024-05-24 00:17:24 -0400

    late

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -4,9 +4,12 @@ import './Landing.css'
 const Landing = () => {
     return (
         <main className='landing-main-container flex flex-col items-center justify-center'>
-            <header className='landing-header flex text-[10rem]'>LED DETROIT</header>
+            <header className='landing-header flex text-7xl md:text-[10rem]'>LED DETROIT</header>
             <img src='./ZeppelinLogoSeven.png' className='flex w-96 h-auto' />
-            <h2 className='landing-description text-7xl'>DETROIT&apos;S <span>ULTIMATE </span><span className='landing-description'>LED </span><span>ZEPPELIN </span><span className='landing-description'>EXPERIENCE</span></h2>
+            <article className='hidden md;flex flex-col items-center my-10'>
+                <h2 className='landing-description text-6xl lg:texg-7xl pb-10'>DETROIT&apos;S ULTIMATE LED ZEPPELIN</h2>
+                <h2 className='landing-description text-6xl lg:texg-7xl pb-10'>EXPERIENCE</h2>
+            </article>
         </main>
     )
 }
