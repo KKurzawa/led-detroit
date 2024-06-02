d5c6bfaa38f8245fc5fb5dbb310c861300e27bed
Kris Kurzawa <kurzawa1@yahoo.com>
2024-06-02 19:24:04 -0400

    fixed landing photo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 8000);
+    }, 80000);
   }, []);
 
   if (isLoading) {
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     return (
         <main className='landing-main-container flex flex-col items-center justify-center mt-10'>
             <header className='landing-header flex text-7xl md:text-[10rem]'>LED DETROIT</header>
-            <img src='./ZeppelinLogoSeven.png' className='landing-logo flex w-96 h-auto' />
+            <img src='./ZeppelinSymbolEight.png' width='750' height='570' className='landing-logo flex w-96 h-auto' />
             <article className='hidden md:flex flex-col items-center my-10'>
                 <h2 id='detroit' className='landing-description text-6xl lg:texg-7xl pb-10'>DETROIT&apos;S<span id='ultimate'> ULTIMATE</span><span id='led'> LED</span><span id='zeppelin'> ZEPPELIN</span></h2>
                 <h2 id='experience' className='landing-description text-6xl lg:texg-7xl pb-10'>EXPERIENCE</h2>
