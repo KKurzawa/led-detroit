1ed66bd7bd3352c8dac4d31f9821fcc745d41cc1
Kris Kurzawa <kurzawa1@yahoo.com>
2024-07-10 18:53:59 -0400

    updated background

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -2,10 +2,12 @@ import './Video.css'
 
 const Video = () => {
     return (
-        <main className='video-main flex justify-center w-full'>
+        <main className='video-main flex justify-center'>
             {/* ro video */}
-            <iframe className='ro-video my-10 hidden md:block rounded-3xl' width="560" height="315" src="https://www.youtube.com/embed/bD2gSjA9M8Y?si=kaVRPyGI2FMD4ze9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <iframe className='ro-video my-10 md:hidden rounded-3xl' width="350" height="197" src="https://www.youtube.com/embed/bD2gSjA9M8Y?si=kaVRPyGI2FMD4ze9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <article className='single-video-container flex justify-center my-10 rounded-3xl'>
+                <iframe className='ro-video hidden md:block rounded-3xl' width="560" height="315" src="https://www.youtube.com/embed/bD2gSjA9M8Y?si=kaVRPyGI2FMD4ze9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe className='ro-video md:hidden rounded-3xl' width="350" height="197" src="https://www.youtube.com/embed/bD2gSjA9M8Y?si=kaVRPyGI2FMD4ze9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </article>
         </main>
     )
 }
