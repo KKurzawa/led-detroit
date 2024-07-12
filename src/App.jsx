8de7c91513d42359739b8928e7b59942e99ffb41
Kris Kurzawa <kurzawa1@yahoo.com>
2024-07-12 14:56:45 -0400

    updated medium and small landing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 8000);
+    }, 7000);
   }, []);
 
   if (isLoading) {
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -22,34 +22,34 @@ const Landing = () => {
             </main>
             {/* medium */}
             <main className='medium-crop-circle-container hidden md:flex lg:hidden flex-col items-center pt-40 pb-64'>
-                <article className='medium-welcome-to flex flex-row justify-center items-center text-4xl -mb-[14.5rem]'>
-                    <h2 className='medium-welcome pr-2'>Welcome </h2>
-                    <h2 className='medium-to'> To</h2>
+                <article className='medium-welcome-to flex flex-row justify-center items-center text-7xl font-bold -mb-[14.5rem]'>
+                    <h2 className='medium-welcome pr-2'>WELCOME</h2>
+                    <h2 className='medium-to'> TO</h2>
                 </article>
                 <article className='medium-led-detroit-container flex justify-center items-center -mb-[17rem]'>
                     <h2 className='medium-led-detroit text-8xl font-bold'>LED-DETROIT</h2>
                 </article>
-                <article className='medium-description flex flex-row justify-center items-center text-3xl'>
-                    <h2 className='medium-detroits pr-2'>Detroits</h2>
-                    <h2 className='medium-ultimate pr-2'>Ultimate</h2>
-                    <h2 className='medium-led-zeppelin pr-2'>Led-Zeppelin</h2>
-                    <h2 className='medium-experience-word'>Experience</h2>
+                <article className='medium-description flex flex-row justify-center items-center text-3xl font-bold'>
+                    <h2 className='medium-detroits pr-3'>DETROITS&apos;S</h2>
+                    <h2 className='medium-ultimate pr-3'>ULTIMATE</h2>
+                    <h2 className='medium-led-zeppelin pr-3'>LED-ZEPPELIN</h2>
+                    <h2 className='medium-experience-word'>EXPERIENCE</h2>
                 </article>
             </main>
             {/* small */}
             <main className='small-crop-circle-container flex md:hidden flex-col items-center pt-40 pb-60'>
-                <article className='small-welcome-to flex flex-row justify-center items-center text-xl -mb-[9rem]'>
-                    <h2 className='small-welcome pr-2'>Welcome </h2>
-                    <h2 className='small-to'> To</h2>
+                <article className='small-welcome-to flex flex-row justify-center items-center text-3xl font-bold -mb-[9rem]'>
+                    <h2 className='small-welcome pr-2'>WELCOME</h2>
+                    <h2 className='small-to'> TO</h2>
                 </article>
                 <article className='small-led-detroit-container flex justify-center items-center -mb-[12rem]'>
-                    <h2 className='small-led-detroit text-5xl font-bold'>LED-DETROIT</h2>
+                    <h2 className='small-led-detroit text-6xl font-bold'>LED-DETROIT</h2>
                 </article>
-                <article className='small-description flex flex-row justify-center items-center text-xl'>
-                    <h2 className='small-detroits pr-1'>Detroits</h2>
-                    <h2 className='small-ultimate pr-1'>Ultimate</h2>
-                    <h2 className='small-led-zeppelin pr-1'>Led-Zeppelin</h2>
-                    <h2 className='small-experience-word'>Experience</h2>
+                <article className='small-description flex flex-row justify-center items-center text-xl font-bold'>
+                    <h2 className='small-detroits pr-2'>DETROIT&apos;S</h2>
+                    <h2 className='small-ultimate pr-2'>ULTIMATE</h2>
+                    <h2 className='small-led-zeppelin pr-2'>LED-ZEPPELIN</h2>
+                    <h2 className='small-experience-word'>EXPERIENCE</h2>
                 </article>
             </main>
         </>
