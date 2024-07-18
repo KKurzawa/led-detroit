deeb0f5752ce34eb3fc81e8e8c80387b9b1056f9
Kris Kurzawa <kurzawa1@yahoo.com>
2024-07-18 18:02:16 -0400

    added social icons to footer

---
+++ src/Utils/SocialMediaLinks.jsx
@@ -0,0 +1,23 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+
+const socialMedia = [
+    {
+        id: 1,
+        icon: <FaFacebookSquare />,
+        link: 'https://www.facebook.com/profile.php?id=61562263779789'
+    },
+    {
+        id: 2,
+        icon: <FaInstagramSquare />,
+        link: ''
+    },
+    {
+        id: 3,
+        icon: <IoLogoYoutube />,
+        link: ''
+    },
+]
+
+export default socialMedia
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import './Footer.css'
+import socialMedia from '../../Utils/SocialMediaLinks';
 
 const Footer = () => {
     const today = new Date();
     return (
         <footer className='footer flex flex-col items-center text-xl py-2'>
+            <ol className='flex justify-center'>
+
+                {socialMedia.map((item) => (
+                    <li key={item.id} className='social-icon'>
+                        <i><a href={item.link} target='__blank'>{item.icon}</a></i></li>
+                ))}
+
+            </ol>
             <a href='mailto:kurzawa1@yahoo.com' className='mailto py-[.1rem]'>Email Us</a>
             <a href='tel:12485682806' className='phone py-[.1rem]'>Call Us</a>
             <p className='copyright py-[.1rem]'>Copyright &copy; {today.getFullYear()} Led Detroit - All Rights Reserved</p>
