7db5315a01fc2bcdc4ff694e391751a8659b1407
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-23 14:20:08 -0400

    updated shows

--- src/Utils/ShowList.jsx
+++ src/Utils/ShowList.jsx
@@ -1,17 +1,10 @@
 const showList = [
     {
-        date: 'August 23 2024 7pm',
-        venue: 'Stony Creek Metropark',
-        location: 'Shelby Twp MI',
-        venueLink: 'https://www.metroparks.com/concerts/',
-        ticketLink: 2
-    },
-    {
-        date: 'September 11 2024 7pm',
-        venue: "The AMP - Brighton's Ampthitheater on Mill Pond",
-        location: 'Brighton MI',
-        venueLink: 'https://2stonesevents.com/new-events/8/6/2022/turn-up-the-amp-featuring-txnyz-lx3h3-yw8n5-cgxt6-k587j-82br2-ksxcn-w9rtt',
-        ticketLink: 2
+        date: 'January 4 2025 8pm',
+        venue: 'The Blind Owl',
+        location: 'Royal Oak MI',
+        venueLink: 'https://www.blindowlrestaurant.com',
+        ticketLink: 1
     },
     {
         date: 'February 7 2025 8pm',
