339cce77197efbf07480551d4888c1264f5b07a4
Kris Kurzawa <kurzawa1@yahoo.com>
2025-01-20 12:41:15 -0500

    updated shows

--- src/Utils/ShowList.jsx
+++ src/Utils/ShowList.jsx
@@ -2,28 +2,28 @@ const showList = [
     {
         date: 'February 7 2025 8pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
     {
         date: 'February 8 2025 8pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
     {
         date: 'May 24 2025 7:30pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
     {
         date: 'June 28 2025 7:30pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
@@ -37,21 +37,21 @@ const showList = [
     {
         date: 'June 26 2025 7:30pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
     {
         date: 'August 23 2025 7:30pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
     {
         date: 'September 27 2025 7:30pm',
         venue: 'The Cadieux Cafe',
-        location: 'Detroit MI',
+        location: 'Detroit, MI',
         venueLink: 'https://cadieuxcafe.com/',
         ticketLink: 1
     },
