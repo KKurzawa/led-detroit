6bbe12208f8b5f14828c8251c7e79a8a6d59373c
Kris Kurzawa <kurzawa1@yahoo.com>
2025-01-20 12:38:10 -0500

    updated shows

--- src/Utils/ShowList.jsx
+++ src/Utils/ShowList.jsx
@@ -1,12 +1,5 @@
 const showList = [
     {
-        date: 'January 4 2025 8pm',
-        venue: 'The Blind Owl',
-        location: 'Royal Oak MI',
-        venueLink: 'https://www.blindowlrestaurant.com',
-        ticketLink: 1
-    },
-    {
         date: 'February 7 2025 8pm',
         venue: 'The Cadieux Cafe',
         location: 'Detroit MI',
@@ -15,6 +8,48 @@ const showList = [
     },
     {
         date: 'February 8 2025 8pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit MI',
+        venueLink: 'https://cadieuxcafe.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'May 24 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit MI',
+        venueLink: 'https://cadieuxcafe.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'June 28 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit MI',
+        venueLink: 'https://cadieuxcafe.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'June 25 2025 7pm',
+        venue: 'Stony Creek Metropark',
+        location: 'Shelby Twp, MI',
+        venueLink: 'https://www.michigan.org/property/stony-creek-metropark',
+        ticketLink: 2
+    },
+    {
+        date: 'June 26 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit MI',
+        venueLink: 'https://cadieuxcafe.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'August 23 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit MI',
+        venueLink: 'https://cadieuxcafe.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'September 27 2025 7:30pm',
         venue: 'The Cadieux Cafe',
         location: 'Detroit MI',
         venueLink: 'https://cadieuxcafe.com/',
