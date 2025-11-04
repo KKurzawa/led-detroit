142843ba3e15d27b23cc1c05c331fc5f326041b1
Kris Kurzawa <kurzawa1@yahoo.com>
2025-11-03 20:37:59 -0500

    updated shows

--- src/Utils/ShowList.jsx
+++ src/Utils/ShowList.jsx
@@ -1,40 +1,5 @@
 const showList = [
     {
-        date: 'June 28 2025 7:30pm',
-        venue: 'The Cadieux Cafe',
-        location: 'Detroit, MI',
-        venueLink: 'https://cadieuxcafe.com/',
-        ticketLink: 1
-    },
-    {
-        date: 'July 25 2025 7pm',
-        venue: 'Stony Creek Metropark',
-        location: 'Shelby Twp, MI',
-        venueLink: 'https://www.michigan.org/property/stony-creek-metropark',
-        ticketLink: 2
-    },
-    {
-        date: 'July 26 2025 7:30pm',
-        venue: 'The Cadieux Cafe',
-        location: 'Detroit, MI',
-        venueLink: 'https://cadieuxcafe.com/',
-        ticketLink: 1
-    },
-    {
-        date: 'August 23 2025 7:30pm',
-        venue: 'The Cadieux Cafe',
-        location: 'Detroit, MI',
-        venueLink: 'https://cadieuxcafe.com/',
-        ticketLink: 1
-    },
-    {
-        date: 'September 27 2025 7:30pm',
-        venue: 'The Cadieux Cafe',
-        location: 'Detroit, MI',
-        venueLink: 'https://cadieuxcafe.com/',
-        ticketLink: 1
-    },
-    {
         date: 'February 20 2026 8:30pm',
         venue: 'The Cadieux Cafe',
         location: 'Detroit, MI',
